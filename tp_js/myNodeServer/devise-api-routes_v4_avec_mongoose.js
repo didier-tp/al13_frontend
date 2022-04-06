aa32ed6eadad05352ffd4941d99726d16d787853
Didier Defrance <didier@d-defrance.fr>
2022-04-06 19:59:59 +0200

    node/express

---
+++ tp_js/myNodeServer/devise-api-routes_v4_avec_mongoose.js
@@ -0,0 +1,135 @@
+var express = require('express');
+const apiRouter = express.Router();
+
+var devise_dao_mongoose = require('./devise-dao-mongoose');
+var PersistentDeviseModel;
+devise_dao_mongoose.initMongooseWithSchemaAndModel(
+	function(InitializedPersistentDeviseModel) {
+		PersistentDeviseModel=InitializedPersistentDeviseModel
+	}
+);
+
+//exemple URL: http://localhost:8282/devise-api/public/devise/EUR
+apiRouter.route('/devise-api/public/devise/:code')
+.get( async function(req , res  , next ) {
+	var codeDevise = req.params.code;
+	/*
+	PersistentDeviseModel.findById( codeDevise ,
+									    function(err,devise){
+											if(devise==null)
+											   res.status(404).send({ err : 'not found'});
+											else
+										       res.send(devise);
+									   });
+	*/
+	/*
+	devise_dao_mongoose.getDeviseByCode(codeDevise)
+	.then((devise)=> { res.send(devise);} )
+	.catch((error)=> { res.status(404).send(error); } );
+	*/
+	//Variante 3 (avec await dans une fonction préfixée par async):
+	try{
+      let devise = await devise_dao_mongoose.getDeviseByCode(codeDevise);
+	  res.send(devise);
+	}catch(ex){
+		res.status(404).send(ex); 
+	}
+});
+
+//exemple URL: http://localhost:8282/devise-api/public/devise-convert?montant=50&source=EUR&cible=USD
+apiRouter.route('/devise-api/public/devise-convert')
+.get( function(req , res  , next ) {
+	let montant = Number(req.query.montant);
+	let codeDeviseSource = req.query.source;
+	let codeDeviseCible = req.query.cible;
+	let changeDeviseSource ;
+	/*
+	devise_dao_mongoose.getDeviseByCode(codeDeviseSource)
+	.then((deviseSource)=> { changeDeviseSource = deviseSource.change;  
+		return devise_dao_mongoose.getDeviseByCode(codeDeviseCible) ; } )
+	.then((deviseCible)=>{
+		res.send ({ montant : montant , 
+			        source : codeDeviseSource , 
+					cible : codeDeviseCible ,
+		            montantConverti : montant * deviseCible.change / 
+					changeDeviseSource });
+	 })
+	.catch((error)=> { res.status(404).send(error); } );
+	*/
+	Promise.all( [ devise_dao_mongoose.getDeviseByCode(codeDeviseSource) ,
+		           devise_dao_mongoose.getDeviseByCode(codeDeviseCible) ])
+	.then( ([deviseSource , deviseCible] )=> {
+		res.send ({ montant : montant , 
+			source : codeDeviseSource , 
+			cible : codeDeviseCible ,
+			montantConverti : montant * deviseCible.change / 
+			                 deviseSource.change });
+	 })
+	.catch((error)=> { res.status(404).send(error); });
+});
+
+//exemple URL: http://localhost:8282/devise-api/public/devise (returning all devises)
+//             http://localhost:8282/devise-api/public/devise?changeMini=1.05
+apiRouter.route('/devise-api/public/devise')
+.get( function(req , res  , next ) {
+	var changeMini = Number(req.query.changeMini);
+	var criteria=changeMini?{ change: { $gte: changeMini } }:{};
+	PersistentDeviseModel.find(criteria,function(err,devises){
+		   if(err) {
+			   console.log("err="+err);
+	       }
+		   res.send(devises);
+	});//end of find()
+});
+
+// http://localhost:8282/devise-api/private/role-admin/devise en mode post
+// avec { "code" : "mxy" , "nom" : "monnaieXy" , "change" : 123 } dans req.body
+apiRouter.route('/devise-api/private/role-admin/devise')
+.post( function(req , res  , next ) {
+	var nouvelleDevise = req.body;
+	console.log("POST,nouvelleDevise="+JSON.stringify(nouvelleDevise));
+	var persistentDevise = new PersistentDeviseModel(nouvelleDevise)
+	persistentDevise.save ( function(err,savedDevise){
+											 if(err==null)
+											   res.send(nouvelleDevise);
+											 else 
+											   res.status(500).send({err : "cannot insert in database" ,
+											                         cause : err});
+									    });
+});
+
+// http://localhost:8282/devise-api/private/role-admin/devise en mode PUT
+// avec { "code" : "USD" , "nom" : "Dollar" , "change" : 1.123 } dans req.body
+apiRouter.route('/devise-api/private/role-admin/devise')
+.put( function(req , res  , next ) {
+	var newValueOfDeviseToUpdate = req.body;
+	console.log("PUT,newValueOfDeviseToUpdate="+JSON.stringify(newValueOfDeviseToUpdate));
+	const filter = { _id : newValueOfDeviseToUpdate.code }
+	PersistentDeviseModel.updateOne( filter , newValueOfDeviseToUpdate,
+		function(err,opResultObject){
+			//console.log(JSON.stringify(opResultObject))
+			if(err || opResultObject.n == 0){
+				res.status(404).json({ err : "no devise to update with code=" + newValueOfDeviseToUpdate.code });
+			}else{
+					res.send(newValueOfDeviseToUpdate);
+			 }
+	});	//end of updateOne()
+});
+
+// http://localhost:8282/devise-api/private/role-admin/devise/EUR en mode DELETE
+apiRouter.route('/devise-api/private/role-admin/devise/:code')
+.delete( function(req , res  , next ) {
+	var codeDevise = req.params.code;
+	console.log("DELETE,codeDevise="+codeDevise);
+	const filter = { _id : codeDevise }
+	PersistentDeviseModel.deleteOne( filter,
+		function(err,opResultObject){
+			//console.log(JSON.stringify(opResultObject))
+			if(err || opResultObject.n == 0)
+				res.status(404).send({ err : "not found , no delete" } );
+			 else
+				 res.send({ deletedDeviseCode : codeDevise } );
+		 });
+});
+
+exports.apiRouter = apiRouter;
